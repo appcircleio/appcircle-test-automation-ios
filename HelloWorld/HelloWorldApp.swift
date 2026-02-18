e89208e6ba6b77d4128174e619565fed9d1097e4
doruk batur <dbatur4@hotmail.com>
2026-02-18 11:41:49 +0300

    init io.appcircle.testautomation

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello, World!")
+                .font(.title)
+                .padding()
+        }
+    }
+}
